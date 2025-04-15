e333b8551990d9e63d4b8bb866ca7b1bf5c94d00
youssse_a <yousse_a@etna-alternance.net>
2025-04-15 16:48:10 +0200

    modif

--- src/components/MysticRoller.tsx
+++ src/components/MysticRoller.tsx
@@ -2,20 +2,20 @@ import React from "react";
 import "../styles/MysticRoller.css";
 
 const logos = [
-  { src: "./images/lgoTS.png", name: "Typescript" },
-  { src: "./images/logoJS.png", name: "Javascript" },
-  { src: "./images/lgoPython.png", name: "Python" },
-  { src: "./images/logoGolang.png", name: "Golang" },
-  { src: "./images/logoHTML.png", name: "HTML" },
-  { src: "./images/logoCSS.png", name: "CSS" },
-  { src: "./images/logoMySQL.png", name: "MySQL" },
-  { src: "./images/logoPostgre.png", name: "PostgreSQL" },
-  { src: "./images/logoReact.png", name: "ReactJS" },
-  { src: "./images/logoNest.png", name: "NestJS" },
-  { src: "./images/logoNotion.png", name: "Nation" },
-  { src: "./images/logoFigma.png", name: "Figma" },
-  { src: "./images/lgoMicrosoftAzur.png", name: "Microsoft Azur" },
-  { src: "./images/lgoArduino.png", name: "Arduino" },
+  { src: "lgoTS.png", name: "Typescript" },
+  { src: "logoJS.png", name: "Javascript" },
+  { src: "lgoPython.png", name: "Python" },
+  { src: "logoGolang.png", name: "Golang" },
+  { src: "logoHTML.png", name: "HTML" },
+  { src: "logoCSS.png", name: "CSS" },
+  { src: "logoMySQL.png", name: "MySQL" },
+  { src: "logoPostgre.png", name: "PostgreSQL" },
+  { src: "logoReact.png", name: "ReactJS" },
+  { src: "logoNest.png", name: "NestJS" },
+  { src: "logoNotion.png", name: "Nation" },
+  { src: "logoFigma.png", name: "Figma" },
+  { src: "lgoMicrosoftAzur.png", name: "Microsoft Azur" },
+  { src: "lgoArduino.png", name: "Arduino" },
 ];
 
 const MysticRoller: React.FC = () => {
